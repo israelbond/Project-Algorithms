42f1af99829bed501f487dcc77763b8941fda393
israelbond <ibond@pdx.edu>
2016-10-29 10:23:51 -0700

    new file:   .project 	new file:   list.cpp 	new file:   list.h 	new file:   main.cpp

---
+++ list.cpp
@@ -0,0 +1,43 @@
+#include "list.h"
+
+RBTree::RBTree()
+{
+   root = NULL;
+}
+
+RBTree::~RBTree()
+{
+  if(root) return DeleteTree(root);
+}
+
+void RBTree::Insert(int num)
+{
+}
+
+void RBTree::Remove(int num)
+{
+}
+
+void RBTree::Search(int num)
+{
+}
+
+void RBTree::DisplayOne()
+{
+}
+
+void RBTree::DisplayTree()
+{
+	if(root) DisplayTree(root);
+}
+
+void RBTree::Display(RBNode *& root)
+{
+	if(!root) return;
+	Display(root->left);
+	cout <<"\t Data: "<< root->data <<"\t Color: "<< color"\n";
+	Display(root->right);
+
+}
+
+RBTree::
---
+++ list.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct RBNode
+{
+	char color;
+	int data;
+
+	RBNode * left;
+	RBNode * right;
+	
+};
+
+class RBTree
+{
+	public:
+	RBTree();
+	~RBTree();
+	void Insert(int);
+	void Remove(int);
+	void search(int);
+	void DisplayOne();
+	void DisplayTree();
+
+	private:
+	RBNode * root;
+
+	void DisplayTree(RBNode *&);
+
+};
